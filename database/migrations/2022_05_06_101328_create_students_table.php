6e2ac273fab230e4c4adfb1dfddce8ec3dc41f63
Adrian Del Moral <iFullStart@gmail.com>
2022-05-06 14:11:40 +0200

    cambios en relaciones(tabla de pedidos)

--- database/migrations/2022_05_06_101328_create_students_table.php
+++ database/migrations/2022_05_06_101328_create_students_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('requests', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("user_id")->nullable()->constrained();
-            $table->boolean('estado_pago');
+            //$table->foreignId("user_id")->nullable()->constrained();
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('requests');
+        Schema::dropIfExists('students');
     }
 };
