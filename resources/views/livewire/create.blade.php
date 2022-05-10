7a183a88ebe9fa471567e56ca4fff7ab2f80c639
Adrian Del Moral <iFullStart@gmail.com>
2022-05-10 11:48:17 +0200

    Revert "preLivewire web fixed"
    This reverts commit 5ed08055d2e06e41f6f8f9c2b3cfc73cb86818e8.

--- resources/views/livewire/create.blade.php
+++
@@ -1,8 +0,0 @@
-<h2>Nueva Plataforma</h2>
-{{-- La vista de creacion tiene aqui una config basica --}}
-@include('livewire.form') {{-- y dentro el formulario --}}
-
-{{-- Cuanto presione el boton guardar, usara el metodo store que esta en app\Http\Livewire\PlatformComponent --}}
-<button wire:click="store" class="btn btn-primary">
-    Guardar
-</button>
