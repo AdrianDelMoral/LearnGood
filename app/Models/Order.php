079e0f7291c82c17428acdce961aef106fbb991b
Adrian Del Moral <iFullStart@gmail.com>
2022-05-24 20:59:05 +0200

    .

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,18 +9,6 @@ class Order extends Model
 {
     use HasFactory;
 
-    public function getAlumno()
-    {                           // Obtiene la id del alumno, y obtiene los profesores solicitados
-        return $this->belongsTo(User::class, 'user_id_alumno');
-    }
-
-    public function getProfesor()
-    {                           // Obtiene la id del profesor, y obtiene los profesores solicitados
-        return $this->belongsTo(User::class, 'user_id_profesor');
-    }
-
-    protected $table = 'orders';
-
     protected $fillable = [
         'user_id_profesor',
         'user_id_alumno',
@@ -28,14 +16,19 @@ class Order extends Model
         'status',
     ];
 
+    protected $table = 'orders';
+    public function getAlumno()
+    {   // Obtiene la id del alumno, y obtiene los profesores solicitados
+        return $this->belongsTo(User::class, 'user_id_alumno');
+    }
+
+    public function getProfesor()
+    {   // Obtiene la id del profesor, y obtiene los profesores solicitados
+        return $this->belongsTo(User::class, 'user_id_profesor');
+    }
+
     public function cursoModel(){
         return $this->belongsTo(Course::class, 'courses_id');
     }
 
-    // Luego podrás relacionar los usuarios:
-        // $usuario_seguido->seguidores()->attach($usuarioId);
-
-    // Y obtener los resultados:
-        // $seguidores = $user->seguidores;
-        // $seguidos = $user->seguidos;
 }
